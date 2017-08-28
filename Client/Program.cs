b101c8a9972cbc7a3b3c05fc7d6aaad4871ed97b
gwarner3 <george@georgedevelops.com>
2017-08-28 08:34:08 -0500

    Removed unused using statements, adjusted white spaces, and removed naming of Main thread.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading;
 using System.Windows.Forms;
-
-
-
 
 namespace Client
 {
@@ -15,10 +7,8 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Thread.CurrentThread.Name = "Main";
             Client client = new Client(IPFinder.GetLocalIPAddress(), 9999);
             Application.Run(client.chatroom);
-
         }
     }
 }
